29d892cbab18c532bfb5da32d006ed42d4f7fa6b
Rahul Jadhav <rahooligan@gmail.com>
2015-03-21 15:54:00 +0000

    initial commit

---
+++ js/data-template.js
@@ -0,0 +1,10 @@
+var data = {
+    "user": "First Last",
+    "feeling": "happy | okay | notokay | unhappy",
+    "intensity-happy": 3,
+    "intensity-okay": 5,
+    "intensity-not-okay": 5,
+    "intensity-unhappy": 8,
+    "coments": "",
+    "timestamp": 0
+}
